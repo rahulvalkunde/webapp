42fa179eab1ff7bad7500188cc3388bfbeaaac3f
root <root@ip-172-31-45-75.ap-south-1.compute.internal>
2021-04-14 14:26:46 +0000

    first commit

---
+++ src/main/java/com/rahulcompany/software/App.java
@@ -0,0 +1,13 @@
+package com.rahulcompany.software;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
